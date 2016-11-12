7244c74640cb220cd5eeabe7158a776d42f09f0c
Venkata8 <mantena.venkata@gmail.com>
2016-11-12 17:53:49 -0500

    updated

--- public/scripts/modules/sample-module/dashboard-controller.js
+++ public/scripts/modules/sample-module/dashboard-controller.js
@@ -1,7 +1,7 @@
 define(['angular', './sample-module'], function (angular, controllers) {
     'use strict';
 
-    // Controller definition
+    // Controllers definition
      controllers.controller('DashboardCtrl', ['$scope','$log', 'PredixAssetService', 'PredixViewService','$http','$interval','$compile','$window', function ($scope,$log, PredixAssetService, PredixViewService,$http,$interval,$compile,$window) {
 
 //  map options
